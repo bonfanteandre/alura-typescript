acf530be9d5de737ab0abb2335d35562b00220bf
= <andre@bwcenter.com.br>
2020-04-20 10:12:08 -0300

    Criando decorator para injetar elementos do DOM

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,21 +1,24 @@
 import { Negociacao, Negociacoes } from '../models/index';
 import { NegociacoesView, MensagemView } from '../views/index';
 import { medirTempoDeExecucao } from '../helpers/decorators/index';
+import { domInject } from '../helpers/decorators/index';
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+
+    @domInject('#valor')
     private _inputValor: JQuery;
+
     private _negociacoes: Negociacoes = new Negociacoes();
     private _negociacoesView: NegociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView: MensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade= $('#quantidade');
-        this._inputValor = $('#valor');
-
         this._negociacoesView.update(this._negociacoes);
     }
 
---
+++ app/ts/helpers/decorators/domInject.ts
@@ -0,0 +1,20 @@
+export function domInject(seletor: string) {
+    return function(target: any, propertyKey: string) {
+        
+        let elemento: JQuery;
+
+        const getter = function() {
+
+            if (!elemento) {
+                console.log(`Busca ${seletor} para injetar em ${propertyKey}`);
+                elemento = $(seletor);
+            }
+
+            return elemento;
+        }
+
+        Object.defineProperty(target, propertyKey, {
+            get: getter
+        });
+    }
+}
